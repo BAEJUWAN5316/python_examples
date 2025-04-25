3f2b19a90163ab1cb0d66587361c40bf64466d19
juwan.bae <vitr5316@naver.com>
2025-04-25 16:50:54 +0900

    1일차 완료

--- 250425/first.py
+++ 250425/first.py
@@ -9,12 +9,71 @@
 '''
 # ㅇㄹㅇㄹ
 
+'''
 print("김치볶음밥은 마시쩡쩡")
 print("김아무개 와우와우 김치볶음밥")
+'''
+'''
+print("☆ ☆ ☆ ☆ ☆ ☆ ☆")
+print("☆           ☆")
+print("☆           ☆")
+print("☆           ☆")
+print("☆           ☆")
+'''
+
+# 변수
 
 
+# 변수이름 = 값
+
+name = "김치찌개"
+name = "김치볶음밥"
+# 변수이름은 의미있어야한다.
+
+'''
+name = "김치찌개"
+print(name)
+name = "김치볶음밥"
+print(name)
+'''
+
+age = 10 # 숫자값
+
+test = 10
+
+Test = 20
+
+# 1test 안 된다
+# te st 안 된다
+
+'''
+fruit_1 = "사과"
+fruit_2 = "바나나"
+fruit_3 = "파인애플"
+fruit_4 = "배"
+fruit_5 = "포도"
+print(fruit_1, fruit_2, fruit_3, fruit_4, fruit_5)
+'''
+
+num_1 = "10"
+num_2 = "20"
+text_1 = "+"
+text_2 = "="
+print(num_1,text_1,num_1,text_2,num_2)
+print(num_1,"+",num_1,"=",num_2)
+
+num_1 = 10
+num_2 = 20
+print(num_1,"+",num_1,"=",num_1+num_1)
+
+# 대문자로 이루어진 변수는 상수로 취급! *실제론 아니지만 개발자들끼리의 약속
+
+PI = 3.14
+PI = 2
 
 
-
+# 타입 체크
+print(type(PI))
+print(type(name))
 
 
