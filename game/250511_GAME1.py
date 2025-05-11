367986621b4a88129cd5e62c1416a1fe91070a89
juwan.bae <vitr5316@naver.com>
2025-05-12 02:42:05 +0900

    250512 게임업뎃

--- game/250511_GAME1.py
+++ game/250511_GAME1.py
@@ -12,7 +12,8 @@ level1 = 1
 
 job1 = "전사"
 status1 = {"힘":10, "민첨":10, "경험치":0}
-point1 = {"체력":50, "마력":20}
+point1 = {"체력":50, "마력":20} # 깎여야 할 체력
+point2 = {"체력":50, "마력":20} # 현재 총 체력
 attack1 = [5, 6, 7, 8, 9, 12]
 inven1 = {"무기":["없음"], "소모품":["없음"]}
 money1 = 0
@@ -28,11 +29,10 @@ drink = {"빨간물약":20, "파란물약":10}
 
 
 # 몬스터
-mon_list = ["다람쥐", "토끼"]
-mon1 = {"다람쥐":{"체력":15, "공격력":[1, 2, 3, 5], "경험치":10, "돈":10}}
-mon2=  {"토끼":{"체력":10, "공격력":[1, 2,], "경험치":5, "돈":10}}
-
-battle1 = random.choice(mon_list)
+mon_list = ["다람쥐", "토끼", "늑대"]
+mon1 = {"다람쥐":{"체력":15, "공격력":[1, 2, 3, 5], "경험치":10, "돈":10, "리셋체력":15}}
+mon2=  {"토끼":{"체력":10, "공격력":[1, 2], "경험치":5, "돈":10, "리셋체력":10}}
+mon3=  {"늑대":{"체력":30, "공격력":[4, 5, 6, 8], "경험치":30, "돈":30, "리셋체력":30}}
 
 
 # 메세지
@@ -89,7 +89,7 @@ while True:
     print(" ")
     print(f"[현재 상태]")
     print(f"[레벨: {level1}]")
-    print(f"[체력: {point1["체력"]} 마력: {point1["마력"]} 돈: {money1}]")
+    print(f"[체력: {point1["체력"]} 마력: {point1["마력"]} 돈: {money1}] 경험치: {status1['경험치']}")
     message1 = input("플레이어의 행동을 고르세요 [1.사냥하기/2.상점가기/3.상태보기/4.끝내기]")
 
     if message1 == "1":
@@ -99,6 +99,8 @@ while True:
                 enemy1 = mon1
             elif battle1 == "토끼":
                 enemy1 = mon2
+            elif battle1 == "늑대":
+                enemy1 = mon3
             
             if message2 == "1":
                 print("플레이어는 공격을 했다!")
@@ -120,6 +122,7 @@ while True:
             elif message2 == "3":
                 print(" ")
                 print("플레이어는 도망쳤다!")
+                enemy1[list(enemy1.keys())[0]]["체력"] = enemy1[list(enemy1.keys())[0]]["리셋체력"]
                 break
 
             if enemy1[list(enemy1.keys())[0]]["체력"] <= 0:
@@ -127,20 +130,33 @@ while True:
                 print(f"{list(enemy1.keys())[0]}은 쓰러졌다!")
                 status1["경험치"] += enemy1[list(enemy1.keys())[0]]["경험치"]
                 money1 += enemy1[list(enemy1.keys())[0]]["돈"]
+                enemy1[list(enemy1.keys())[0]]["체력"] = enemy1[list(enemy1.keys())[0]]["리셋체력"]
                 print(f"플레이어는 경험치를 {enemy1[list(enemy1.keys())[0]]["경험치"]}만큼 획득했다!")
                 print(f"플레이어는 돈을 {enemy1[list(enemy1.keys())[0]]["돈"]}만큼 획득했다!")
 
                 for level2 in range(30,36):
                     if level2 == status1["경험치"]:
                         level1 +=1
+                        point2["체력"] +=10
+                        point2["마력"] +=15
+                        point1["체력"] = point2["체력"]
+                        point1["마력"] = point2["마력"]
                         print(level_up)
                 for level2 in range(100,106):
                     if level2 == status1["경험치"]:
                         level1 +=1
+                        point2["체력"] +=10
+                        point2["마력"] +=15
+                        point1["체력"] = point2["체력"]
+                        point1["마력"] = point2["마력"]
                         print(level_up)
                 for level2 in range(200,206):
                     if level2 == status1["경험치"]:
                         level1 +=1
+                        point2["체력"] +=10
+                        point2["마력"] +=15
+                        point1["체력"] = point2["체력"]
+                        point1["마력"] = point2["마력"]
                         print(level_up)
                 break
 
